84079368d060716bbfd9df2ad096f38ed421f977
sabiradnan90 <sabiradnan00@gmail.com>
2025-11-05 11:26:16 +0500

    files edited

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
+}
+
+# ------------------------------
+# Random ID for uniqueness
+# ------------------------------
+resource "random_id" "rand" {
+  byte_length = 4
 }
 
 # ------------------------------
@@ -11,31 +31,42 @@ resource "azurerm_resource_group" "main" {
 }
 
 # ------------------------------
-# 2️⃣ Storage Account for static files
+# 2️⃣ Storage Account + Blob container
 # ------------------------------
 resource "azurerm_storage_account" "static" {
-  name                     = "crcstorage${random_id.rand_id.hex}"  # globally unique
+  name                     = "crcstorage${random_id.rand.hex}"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-# Blob container for static files
 resource "azurerm_storage_container" "static" {
   name                  = "static"
   storage_account_name  = azurerm_storage_account.static.name
   container_access_type = "blob"
 }
 
-# Random ID for unique names
-resource "random_id" "rand_id" {
-  byte_length = 4
+# ------------------------------
+# 3️⃣ Cosmos DB
+# ------------------------------
+resource "azurerm_cosmosdb_account" "main" {
+  name                = "crccosmos${random_id.rand.hex}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.main.location
+    failover_priority = 0
+  }
 }
 
 # ------------------------------
-# 3️⃣ Cosmos DB Account
+# 4️⃣ Function App Plan
 # ------------------------------
-resource "azurerm_cosmosdb_account" "main" {
-  name                = "crccosmos${random_id.rand_id.hex}"
-  location            = azurerm_resource_group.main.l
+resource "azurerm_app_service_plan" "pl_
