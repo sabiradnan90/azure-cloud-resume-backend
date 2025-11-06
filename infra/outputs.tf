e402c9ee6389aae2ba5ca58233b14941e2994b6a
sabiradnan90 <sabiradnan00@gmail.com>
2025-11-06 08:33:53 +0500

    main file edited

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -11,13 +11,13 @@ output "resource_group_name" {
 # Function App Name
 output "function_app_name" {
   description = "Name of the Azure Function App"
-  value       = azurerm_function_app.main.name
+  value       = azurerm_function_app.visitor.name
 }
 
 # Blob Storage Account Name
 output "blob_account_name" {
   description = "Name of the Azure Storage Account used for static website hosting"
-  value       = azurerm_storage_account.main.name
+  value       = azurerm_storage_account.static.name
 }
 
 # Cosmos DB Account Name
@@ -33,13 +33,13 @@ output "cosmosdb_account_name" {
 # Function App Default URL
 output "function_app_url" {
   description = "Default URL endpoint of the deployed Function App"
-  value       = "https://${azurerm_function_app.main.default_hostname}/"
+  value       = "https://${azurerm_function_app.visitor.default_hostname}/"
 }
 
 # Storage Account Web Endpoint
 output "storage_web_endpoint" {
   description = "Web endpoint of the static website hosted in Azure Blob Storage"
-  value       = azurerm_storage_account.main.primary_web_endpoint
+  value       = azurerm_storage_account.static.primary_web_endpoint
 }
 
 # Cosmos DB Endpoint
