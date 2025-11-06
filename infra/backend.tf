598751a08bd7b55b4031498e9c614250ff9ff32a
sabiradnan90 <sabiradnan00@gmail.com>
2025-11-06 09:00:34 +0500

    yml file edited

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-backend-rg"       # replace with your RG
-    storage_account_name = "terraformbackendstorage"    # replace with your storage account
+    resource_group_name  = "Devops"
+    storage_account_name = "terraformbackendstorage"
     container_name       = "tfstate"
     key                  = "cloud-resume.tfstate"
   }
