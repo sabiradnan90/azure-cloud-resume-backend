6a3867fe68eb3bc2b67c651624bb0537e3f705e1
sabiradnan90 <sabiradnan00@gmail.com>
2025-11-06 08:27:44 +0500

    yml file edited

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,15 +1,49 @@
+###########################################
+# Terraform Outputs - Azure Cloud Resume
+###########################################
+
+# Resource Group Name
 output "resource_group_name" {
-  value = azurerm_resource_group.main.name
+  description = "Name of the Azure Resource Group"
+  value       = azurerm_resource_group.main.name
 }
 
+# Function App Name
 output "function_app_name" {
-  value = azurerm_function_app.main.name
+  description = "Name of the Azure Function App"
+  value       = azurerm_function_app.main.name
 }
 
+# Blob Storage Account Name
 output "blob_account_name" {
-  value = azurerm_storage_account.main.name
+  description = "Name of the Azure Storage Account used for static website hosting"
+  value       = azurerm_storage_account.main.name
 }
 
+# Cosmos DB Account Name
 output "cosmosdb_account_name" {
-  value = azurerm_cosmosdb_account.main.name
+  description = "Name of the Azure Cosmos DB Account"
+  value       = azurerm_cosmosdb_account.main.name
+}
+
+###########################################
+# Optional: Additional Helpful Outputs
+###########################################
+
+# Function App Default URL
+output "function_app_url" {
+  description = "Default URL endpoint of the deployed Function App"
+  value       = "https://${azurerm_function_app.main.default_hostname}/"
+}
+
+# Storage Account Web Endpoint
+output "storage_web_endpoint" {
+  description = "Web endpoint of the static website hosted in Azure Blob Storage"
+  value       = azurerm_storage_account.main.primary_web_endpoint
+}
+
+# Cosmos DB Endpoint
+output "cosmosdb_endpoint" {
+  description = "Cosmos DB account endpoint URI"
+  value       = azurerm_cosmosdb_account.main.endpoint
 }
