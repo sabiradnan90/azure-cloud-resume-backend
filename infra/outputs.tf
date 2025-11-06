d4516ae425b597c2fa08e342e93a8d9aa839aaa1
sabiradnan90 <sabiradnan00@gmail.com>
2025-11-06 08:23:41 +0500

    files edited

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,11 +3,11 @@ output "resource_group_name" {
 }
 
 output "function_app_name" {
-  value = azurerm_function_app.visitor.name
+  value = azurerm_function_app.main.name
 }
 
 output "blob_account_name" {
-  value = azurerm_storage_account.static.name
+  value = azurerm_storage_account.main.name
 }
 
 output "cosmosdb_account_name" {
