991468a95ae8c92a1559eeb812e90e2193903bf0
sabiradnan90 <sabiradnan00@gmail.com>
2025-11-09 03:09:44 +0500

    yml file edited

--- function_app/VisitorCounter/__init__.py
+++ function_app/VisitorCounter/__init__.py
@@ -2,27 +2,21 @@ import os
 import json
 from azure.cosmos import CosmosClient, exceptions
 
+COSMOS_CONNECTION_STRING = os.environ["COSMOS_CONNECTION_STRING"]
+COSMOS_DB_NAME = os.environ["COSMOS_DB_NAME"]
+COSMOS_CONTAINER_NAME = os.environ["COSMOS_CONTAINER_NAME"]
+
+client = CosmosClient.from_connection_string(COSMOS_CONNECTION_STRING)
+database = client.get_database_client(COSMOS_DB_NAME)
+container = database.get_container_client(COSMOS_CONTAINER_NAME)
+
 def main(req):
-    connection_string = os.environ["COSMOS_CONNECTION_STRING"]
-    db_name = os.environ["COSMOS_DB_NAME"]
-    container_name = os.environ["COSMOS_CONTAINER_NAME"]
-
-    client = CosmosClient.from_connection_string(connection_string)
-    database = client.get_database_client(db_name)
-    container = database.get_container_client(container_name)
-
     try:
-        # Try to read existing counter item
         item = container.read_item(item="counter", partition_key="counter")
         item["count"] += 1
         container.replace_item(item="counter", body=item)
     except exceptions.CosmosResourceNotFoundError:
-        # If not found, create the first counter
         item = {"id": "counter", "count": 1}
         container.create_item(body=item)
-
-    return {
-        "status": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": json.dumps({"visitor_count": item["count"]})
-    }
+    
+    return json.dumps({"visitor_count": item["count"]})
