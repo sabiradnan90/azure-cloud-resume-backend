26b816fbf5e7b914e70d14c3dfe068072aa28242
sabiradnan90 <sabiradnan00@gmail.com>
2025-11-08 21:44:57 +0500

    New files added

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "Devops"
-    storage_account_name = "myterrfaormstorage"  # updated with your actual storage account
-    container_name       = "tfstate"
-    key                  = "cloud-resume.tfstate"
-  }
-}
