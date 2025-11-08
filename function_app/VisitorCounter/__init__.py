c0641f4d999c7c445154d8470d6dcbefde18d115
sabiradnan90 <sabiradnan00@gmail.com>
2025-11-09 02:05:02 +0500

    yml file edited

--- function_app/VisitorCounter/__init__.py
+++ function_app/VisitorCounter/__init__.py
@@ -1,26 +1,22 @@
-import logging
-import azure.functions as func
-from azure.cosmos import CosmosClient
 import os
+import json
+from azure.cosmos import CosmosClient, PartitionKey, exceptions
 
-COSMOS_ACCOUNT = os.environ['COSMOS_DB_ACCOUNT']
-DATABASE_NAME = os.environ['COSMOS_DB_DATABASE']
-CONTAINER_NAME = os.environ['COSMOS_DB_CONTAINER']
+COSMOS_CONNECTION_STRING = os.environ['COSMOS_CONNECTION_STRING']
+COSMOS_DB_NAME = os.environ['COSMOS_DB_NAME']
+COSMOS_CONTAINER_NAME = os.environ['COSMOS_CONTAINER_NAME']
 
-# Cosmos Client using connection string
-CONNECTION_STRING = os.environ['COSMOS_CONNECTION_STRING']
-client = CosmosClient.from_connection_string(CONNECTION_STRING)
+client = CosmosClient.from_connection_string(COSMOS_CONNECTION_STRING)
+database = client.get_database_client(COSMOS_DB_NAME)
+container = database.get_container_client(COSMOS_CONTAINER_NAME)
 
-db = client.get_database_client(DATABASE_NAME)
-container = db.get_container_client(CONTAINER_NAME)
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    visitor_id = 'counter'
+def main(req):
     try:
-        item = container.read_item(item=visitor_id, partition_key=visitor_id)
-        item['visits'] += 1
-        container.upsert_item(item)
-    except Exception:
-        item = {'id': visitor_id, 'visits': 1}
-        container.create_item(item)
-    return func.HttpResponse(f"{{'visits': {item['visits']}}}", mimetype='application/json')
+        doc = container.read_item(item='counter', partition_key='counter')
+        doc['count'] += 1
+        container.replace_item(item='counter', body=doc)
+    except exceptions.CosmosResourceNotFoundError:
+        doc = {'id': 'counter', 'count': 1}
+        container.create_item(body=doc)
+    
+    return json.dumps({"visitor_count": doc['count']})
