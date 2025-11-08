e2d44bd582fc193cc2bb98567872ab2c6fa1e07e
sabiradnan90 <sabiradnan00@gmail.com>
2025-11-09 03:50:27 +0500

    yml file edited

--- function_app/VisitorCounter/__init__.py
+++ function_app/VisitorCounter/__init__.py
@@ -1,22 +1,21 @@
-import os
-import json
-from azure.cosmos import CosmosClient, exceptions
+import logging
+import azure.functions as func
 
-COSMOS_CONNECTION_STRING = os.environ["COSMOS_CONNECTION_STRING"]
-COSMOS_DB_NAME = os.environ["COSMOS_DB_NAME"]
-COSMOS_CONTAINER_NAME = os.environ["COSMOS_CONTAINER_NAME"]
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("VisitorCounter function processed a request.")
 
-client = CosmosClient.from_connection_string(COSMOS_CONNECTION_STRING)
-database = client.get_database_client(COSMOS_DB_NAME)
-container = database.get_container_client(COSMOS_CONTAINER_NAME)
+    name = req.params.get("name")
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            req_body = {}
+        name = req_body.get("name")
 
-def main(req):
-    try:
-        item = container.read_item(item="counter", partition_key="counter")
-        item["count"] += 1
-        container.replace_item(item="counter", body=item)
-    except exceptions.CosmosResourceNotFoundError:
-        item = {"id": "counter", "count": 1}
-        container.create_item(body=item)
-    
-    return json.dumps({"visitor_count": item["count"]})
+    if name:
+        return func.HttpResponse(f"Hello, {name}! Your visit is counted.")
+    else:
+        return func.HttpResponse(
+            "Hello! Your visit is counted.",
+            status_code=200
+        )
